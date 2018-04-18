eed8cf76e604051d00ea20b567a7a0c3e305de07
Thomas Robveille <thomas.ro@hotmail.fr>
2018-04-18 09:15:58 +0200

    init commit

--- src/Controller/luckyController.php
+++ src/Controller/luckyController.php
@@ -5,3 +5,26 @@
  * Date: 16/04/2018
  * Time: 15:35
  */
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class luckyController extends Controller
+{
+    /**
+* @Route("/lucky/number")
+     */
+
+    public function number()
+    {
+        $number = mt_rand(0, 100);
+
+        return $this->render('lucky/number.html.twig', array(
+            'number' => $number,
+
+        ));
+    }
+}
